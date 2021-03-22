0154b0fb37a41c304b458427937c8cff2574624b
Igor Cruz <47676774+mjigormj@users.noreply.github.com>
2021-03-22 04:19:42 -0300

    Api Enviando Email
    Agora os Surveys podem ser enviados aos Users

---
+++ src/routes.ts
@@ -0,0 +1,20 @@
+import {Router} from "express";
+import { SendMailController } from "./controllers/SendMailController";
+import { SurveysController } from "./controllers/SurveyController";
+import { UserController } from "./controllers/UserController";
+
+const router = Router();
+
+const userController = new UserController();
+const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
+
+router.post("/users", userController.create);
+router.get("/users", userController.show);
+
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
+
+router.post("/sendMail", sendMailController.execute);
+
+export { router };
